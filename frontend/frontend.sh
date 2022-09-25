8eb7cb5d75b239f3a8639e2ecb42b9ba1f80ebfc
Fahri Gunadi <fahripx0@gmail.com>
2022-09-25 19:21:52 +0700

    frontend

---
+++ frontend/frontend.sh
@@ -0,0 +1,12 @@
+npm i react-router-dom axios bulma
+
+npm start
+
+cd src
+rm App.css App.test.js index.css logo.svg reportWebVitals.js setupTests.js
+mkdir components
+
+cd src/components
+touch UserList.js
+touch AddUser.js
+touch EditUser.js
